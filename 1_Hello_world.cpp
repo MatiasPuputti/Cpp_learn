f4288bbe69940ea397ba2ec9f65eef8e3015a0e8
MatiasPuputti <matias_puputti@hotmail.com>
2020-03-07 21:08:16 +0200

    Gitt ignore and hw program
    Added gitignore and  hello world program.

---
+++ 1_Hello_world.cpp
@@ -0,0 +1,15 @@
+//Header thet defines the standard input/output stream objects.
+#include <iostream> 
+
+// Defines namespace as standard.
+using namespace std;
+
+// Main function.
+int main()
+{
+    // Writing a line.
+    cout << "Hello world!" << endl;
+
+    // The 0 traditionally indicates to the os that the program was run succesfully.
+    return 0;
+}
