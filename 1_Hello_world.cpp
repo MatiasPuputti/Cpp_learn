27c78617c6eb04de744cb9bdb5d45fbb1471de90
MatiasPuputti <matias_puputti@hotmail.com>
2020-03-07 21:52:47 +0200

    Small additions
    Added some variables to the hello world and readme exlusion into gitignore.

--- 1_Hello_world.cpp
+++ 1_Hello_world.cpp
@@ -1,14 +1,21 @@
 //Header thet defines the standard input/output stream objects.
 #include <iostream> 
 
-// Defines namespace as standard.
+// Using directive defines namespace as standard.
+// Namespaces are used to group code, and prevent/work around naming conflicts. 
 using namespace std;
 
 // Main function.
 int main()
 {
+    // Declaration of a variable.
+    int num;
+    
+    // Initialization of the variable.
+    num = 5;
+
     // Writing a line.
-    cout << "Hello world!" << endl;
+    cout << "Hello world!\n";
 
     // The 0 traditionally indicates to the os that the program was run succesfully.
     return 0;
